8d1d3808abc4518024cbecf7717d5c5ed4f67733
Abdelfatah Saad <120993266+abdoooooo90@users.noreply.github.com>
2025-03-11 01:12:44 +0200

    Add project files.

---
+++ IKEA.BLL/Services/DepartmentService.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IKEA.BLL.Models;
+using IKEA.DAL.Models.Departments;
+using IKEA.DAL.Presistance.Repositories.Departments;
+using Microsoft.EntityFrameworkCore;
+
+namespace IKEA.BLL.Services
+{
+    public class DepartmentService : IDepartmentService
+    {
+        private readonly IDepartmentRepository _departmentRepository;
+
+        public DepartmentService(IDepartmentRepository departmentRepository)
+        {
+            _departmentRepository = departmentRepository;
+        }
+
+      
+        public IEnumerable<DepartmentToReturnDto> GetAllDepartments()
+        {
+            var departments = _departmentRepository.GetAllAsQuerable().Select(department=> new DepartmentToReturnDto
+            {
+                Id = department.Id,
+                Code = department.Code,
+                Name = department.Name,
+                
+                CreationDate = department.CreationDate
+            }).AsNoTracking().ToList();
+
+
+
+
+            return departments;
+        }
+
+        public DepartmentDetailsToReturnDto? GetDepartmentById(int id)
+        {
+            var department = _departmentRepository.GetById(id);
+             if (department is { })
+            {
+                return new DepartmentDetailsToReturnDto()
+                {
+                    Id = department.Id,
+                    Code = department.Code,
+                    Name = department.Name,
+                    Description = department.Description,
+
+                    CreationDate = department.CreationDate,
+                    CreatedBy = department.CreatedBy,
+                    CreatedOn = department.CreatedOn,
+                    LastModificationBy = department.LastModificationBy,
+                    LastModificationOn = department.LastModificationOn,
+
+                };
+            }
+            return null;
+            
+        }
+        public int CreateDerpartmenet(CreatedDepartmentDto departmentDto)
+        {
+            var Createddepartment = new Department()
+            {
+                Code = departmentDto.Code,
+                Name = departmentDto.Name,
+                Description = departmentDto.Description,
+                CreationDate = departmentDto.CreationDate,
+                CreatedBy = 1,
+                LastModificationBy = 1,
+                LastModificationOn= DateTime.UtcNow,
+            };
+            return _departmentRepository.Add(Createddepartment);
+        }
+
+        public int UpdateDepartment(UpdateDepartmentDto departmentDto)
+        {
+            var UpdatedDepartment = new Department()
+            {
+                Code = departmentDto.Code,
+                Name = departmentDto.Name,
+                Description = departmentDto.Description,
+                CreationDate = departmentDto.CreationDate,
+
+                LastModificationBy = 1,
+                LastModificationOn = DateTime.UtcNow,
+            };
+            return _departmentRepository.Update(UpdatedDepartment);
+        }
+        public bool DeletedDepartment(int id)
+        {
+            var department = _departmentRepository.GetById(id);
+             if (department is not null)
+            {
+                return _departmentRepository.Delete(department) > 0;
+            }
+            return false;
+        }
+
+        
+    }
+}
---
+++ IKEA.PL/Controllers/DepartmentController.cs
@@ -0,0 +1,216 @@
+﻿using System.Linq.Expressions;
+using System.Security.Policy;
+using IKEA.BLL.Models;
+using IKEA.BLL.Services;
+using IKEA.PL.Models.Departments;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IKEA.PL.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private readonly IDepartmentService _departmentService;
+        private readonly ILogger<DepartmentController> _logger;
+        private readonly IWebHostEnvironment _environment;
+
+       
+        #region Services
+        public DepartmentController(IDepartmentService departmentService, ILogger<DepartmentController> logger, IWebHostEnvironment environment)
+        {
+            _departmentService = departmentService;
+            _logger = logger;
+            _environment = environment;
+            _environment = environment;
+        }
+        #endregion
+        #region Index
+        [HttpGet]
+        //BaseUrl/Department/Index
+        public IActionResult Index()
+        {
+            var departments = _departmentService.GetAllDepartments();
+            return View(departments);
+        }
+        #endregion
+        #region Create
+        #region Get
+        [HttpGet]
+         public IActionResult Create()
+        {
+            return View();
+        }
+        #endregion
+        #region Post
+        [HttpPost]
+         public IActionResult Create(CreatedDepartmentDto department)
+        {
+            if (!ModelState.IsValid)
+                return View(department);
+            var message = string.Empty;
+            try
+            {
+                var result = _departmentService.CreateDerpartmenet(department);
+                if (result > 0)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    message = "Sorry The Department Has Not Been Created";
+                    ModelState.AddModelError(string.Empty, message);
+                    return View(department);
+                }
+                 
+            }
+            
+            catch(Exception ex)
+            {
+                //1- Log Exception
+                _logger.LogError(ex, ex.Message);
+                //2- Set Frindly Message
+                if(_environment.IsDevelopment())
+                {
+                    message = ex.Message;
+                    return View(department);
+                }
+                else
+                {
+                    message = "Sorry The Department Has Not Been Created";
+                    return View("Error", message);
+                }
+
+            }
+        }
+        #endregion
+        #endregion
+        #region Details
+        [HttpGet]
+         public IActionResult Details(int? id)
+        {
+            if(id is null)
+            {
+                return BadRequest();
+            }
+            var department = _departmentService.GetDepartmentById(id.Value);
+            if(department is null)
+            {
+                return NotFound();
+            }
+            return View(department);
+
+        }
+        #endregion
+        #region  Edit
+        #region Get
+        [HttpGet]
+         public IActionResult Edit(int? id)
+        {
+            if (id is null)
+            {
+                return BadRequest();//400
+
+            }
+            var department = _departmentService.GetDepartmentById(id.Value);
+            if(department is null)
+            {
+                return NotFound();//404
+            }
+            var viewModel = new DepartmentEditViewModel()
+            {
+                Id = department.Id,
+                Code = department.Code,
+                Name = department.Name,
+                Description = department.Description,
+                CreationDate = department.CreationDate
+            };
+            return View(viewModel);
+
+        }
+        #endregion
+        #region  Post
+        [HttpPost]
+        public IActionResult Edit (int id, DepartmentEditViewModel departmentVM)
+        {
+            if (!ModelState.IsValid)
+                return View(departmentVM);
+            var message = string.Empty;
+            try
+            {
+                var updatedDepartment = new UpdateDepartmentDto()
+                {
+                    Id = id,
+                    Code = departmentVM.Code,
+                    Name = departmentVM.Name,
+                    Description = departmentVM.Description,
+                    CreationDate = departmentVM.CreationDate
+
+
+                };
+                var updated = _departmentService.UpdateDepartment(updatedDepartment)>0;
+                if(updated)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                message = "Sorry , An Error Occured While Updating The Department";
+
+            }
+             catch(Exception ex)
+            {
+                //1-
+                _logger.LogError(ex, ex.Message);
+                message = _environment.IsDevelopment() ? ex.Message : "Sorry , An Error Occured While Updating The Department";
+
+            }
+            ModelState.AddModelError(string.Empty, message);
+            return View(departmentVM);
+
+
+        }
+        #endregion
+        #endregion
+        #region Delete
+        [HttpGet]
+         public IActionResult Delete (int? id)
+        {
+            if(id is null)
+            {
+                return BadRequest();
+            }
+            var department = _departmentService.GetDepartmentById(id.Value);
+            if(department is null)
+            {
+                return NotFound();
+            }
+            return View(department);
+        }
+        //---------------------
+        [HttpPost]
+         public  IActionResult Delete(int id)
+        {
+            var message = string.Empty;
+            try
+            {
+                var deleted = _departmentService.DeletedDepartment(id);
+                if (deleted)
+                {
+                    return RedirectToAction(nameof(Index));
+
+                }
+                message = "An Error Ocurred During Deleting The Department";
+            }
+             catch(Exception ex)
+            {
+                //1-
+                _logger.LogError(ex, ex.Message);
+                //2- 
+                message = _environment.IsDevelopment() ? ex.Message : "An Error Ocurred During Deleting The Department";
+
+
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        #endregion
+
+
+    } 
+}
